ed2b925eb1a7effd41426af59b6c294cfe702f18
“AnaniyaLegesse“ <ananiya.legesse1@gmail.com>
2023-02-16 12:46:50 -0800

    successful commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: always(0) (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
