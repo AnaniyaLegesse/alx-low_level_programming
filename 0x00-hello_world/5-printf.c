dd07b1a0677a76556782e6123575aab358017fb2
“AnaniyaLegesse“ <ananiya.legesse1@gmail.com>
2023-02-16 12:49:49 -0800

    successful commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - main block
+ *
+ * Return: always(0) (success)
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
